a0bfd76b878c7cc8f06e8cc281179cf88775fff9
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2014-06-23 22:31:53 -0700

    Initial Commit

---
+++ Paper5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paper5
+//
+//  Created by Tyler Miller on 6/23/14.
+//  Copyright (c) 2014 Tyler Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
